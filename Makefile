03828ce5ba6cc3abf9d703ac51b280e3cd4d3ab7
ic3qu33n <v1kt0r_frnknstn@proton.me>
2023-09-04 20:53:38 -0400

    adds improved version of gospel (src asm and binary) w getdents syscalls; adds updated version of test program using readdir calls for dir listing

--- Makefile
+++ Makefile
@@ -2,11 +2,14 @@
 LINKER=x86_64-linux-gnu-ld
 LFLAGS=-static
 src_asm=gospel.asm
-
+src_test=test.c
 
 .PHONY: all
 
 all: gospel
 
 gospel: $(src_asm)
-	nasm -f elf64 $(src_asm) -o gospel.o && $(LINKER) -static gospel.o -o $@
+	nasm -f elf64 $(src_asm) -o gospel.o && $(LINKER) gospel.o -o $@
+
+test: $(src_test)
+	x86_64-linux-gnu-gcc -static -o $@ $(src_test)
--- test.c
+++ test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+
 
 
 int main(){
+	DIR *dir;
+	struct dirent *entry;
+	dir=opendir(".");
+	if (dir == NULL){
+		puts("error opening directory");
+	} else {
+		while ((entry = readdir(dir)) != NULL) {
+			printf(" %s", entry->d_name);
+		}
+	}
 	printf("I am the test executable!\n");
+	closedir(dir);
 	exit(0);
 }
